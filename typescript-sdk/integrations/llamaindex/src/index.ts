b6708a5aa11681b6b09ca84b3de0f063724c4ec6
Markus Ecker <markus.ecker@gmail.com>
2025-07-18 16:07:20 +0200

    update llamaindex doc comment

--- typescript-sdk/integrations/llamaindex/src/index.ts
+++ typescript-sdk/integrations/llamaindex/src/index.ts
@@ -1,6 +1,6 @@
 /**
- * Agno is a framework for building Multi-Agent Systems with memory, knowledge and reasoning.
- * Check more about using Agno: https://docs.agno.com/
+ * LlamaIndex is a simple, flexible framework for building agentic generative AI applications that allow large language models to work with your data in any format.
+ * Check more about using LlamaIndex: https://docs.llamaindex.ai/
  */
 
 import { HttpAgent } from "@ag-ui/client";
