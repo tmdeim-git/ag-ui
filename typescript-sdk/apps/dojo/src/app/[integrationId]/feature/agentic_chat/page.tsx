f5b1a517d0a1e91eabc93706ac019ad9fa2c8297
ran <ran@copilotkit.ai>
2025-07-25 16:48:19 +0300

    chore: fit dojo sidebar and chat width for mobile view

--- typescript-sdk/apps/dojo/src/app/[integrationId]/feature/agentic_chat/page.tsx
+++ typescript-sdk/apps/dojo/src/app/[integrationId]/feature/agentic_chat/page.tsx
@@ -51,7 +51,7 @@ const Chat = () => {
 
   return (
     <div className="flex justify-center items-center h-full w-full" style={{ background }}>
-      <div className="w-8/10 h-8/10 rounded-lg">
+      <div className="h-full w-full md:w-8/10 md:h-8/10 rounded-lg">
         <CopilotChat
           className="h-full rounded-2xl"
           labels={{ initial: "Hi, I'm an agent. Want to chat?" }}
