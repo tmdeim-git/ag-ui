902447f9f260516e1b82be76e0aac393e8ce2f2f
ran <ran@copilotkit.ai>
2025-07-29 17:42:53 +0300

    chore: fit individual demos for mobile

---
+++ typescript-sdk/apps/dojo/src/utils/use-mobile-view.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export function useMobileView() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    return () => window.removeEventListener('resize', checkMobile);
+  }, []);
+
+  return {
+    isMobile,
+  }
+}
