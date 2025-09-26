ba5df137cf1da3163596c17c029a17bcece05b8d
Tyler Slaton <tyler@copilotkit.ai>
2025-09-26 08:29:31 -0700

    test(crew-ai): add fixme for agentic gen-ui
    Signed-off-by: Tyler Slaton <tyler@copilotkit.ai>

--- typescript-sdk/apps/dojo/e2e/tests/crewAITests/agenticGenUI.spec.ts
+++ typescript-sdk/apps/dojo/e2e/tests/crewAITests/agenticGenUI.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { AgenticGenUIPage } from "../../pages/crewAIPages/AgenticUIGenPage";
 
-test.describe("Agent Generative UI Feature", () => {
+test.fixme("[CrewAI] Agentic Gen UI", () => {
   // Flaky
   test("[CrewAI] should interact with the chat to get a planner on prompt", async ({
     page,
@@ -35,7 +35,7 @@ test.describe("Agent Generative UI Feature", () => {
   });
 
   // Flaky
-  test("[CrewAI] should interact with the chat using predefined prompts and perform steps", async ({
+  test.fixme("[CrewAI] should interact with the chat using predefined prompts and perform steps", async ({
     page,
   }) => {
     const genUIAgent = new AgenticGenUIPage(page);
